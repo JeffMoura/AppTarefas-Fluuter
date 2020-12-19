2ee9040009547dde46d764a14db33fc1027cbcc1
Jefferson Moura Git <jeffermouramartins@gmail.com>
2020-12-19 18:20:54 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,10 +16,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _toDoList = [];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-    );
+    return Container();
+  }
+
+//FUNÇÃO PARA OBTER O ARQUIVO
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+//FUNÇÃO PARA SALVAR DADOS NO ARQUIVO
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+//FUNÇÃO PARA LER OS DADOS DO ARQUIVO
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
