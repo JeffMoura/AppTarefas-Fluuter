e4345c0a215f3049c6f8e6c3fd15dc05e69587a0
Jefferson Moura Git <jeffermouramartins@gmail.com>
2020-12-19 18:37:59 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,36 @@ class _HomeState extends State<Home> {
   List _toDoList = [];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.green[300],
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.greenAccent)),
+                  ),
+                ),
+                RaisedButton(
+                    color: Colors.blueGrey,
+                    child: Text("Adicionar"),
+                    textColor: Colors.white,
+                    onPressed: () {})
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
 //FUNÇÃO PARA OBTER O ARQUIVO
@@ -27,6 +56,7 @@ class _HomeState extends State<Home> {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
   }
+
 //FUNÇÃO PARA SALVAR DADOS NO ARQUIVO
   Future<File> _saveData() async {
     String data = json.encode(_toDoList);
@@ -34,6 +64,7 @@ class _HomeState extends State<Home> {
     final file = await _getFile();
     return file.writeAsString(data);
   }
+
 //FUNÇÃO PARA LER OS DADOS DO ARQUIVO
   Future<String> _readData() async {
     try {
